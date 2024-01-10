8e1f489f4c0b8becf9e30375281ae0e3efce8c4e
minamitaga <minami.taga@jp.ricoh.com>
2024-01-10 09:09:42 +0900

    fix ch03/14

--- ch03/ex14/index.js
+++ ch03/ex14/index.js
@@ -1,9 +1,31 @@
 export function eq(a, b) {
-  // TODO: ここを実装しなさい
+
+  if(typeof a === 'string' && typeof b === 'string') {
+    return a === b;
+  }
+  if(a === b) return true;
+  // 数値の比較
+  if (Number(a) === Number(b)) return true;
+  // Dateの比較
+  if (new Date(`${a}`).getTime() === new Date(`${b}`).getTime()) return true;
+  if (String(new Date(`${a}`).getTime()) === String(new Date(b).getTime())) return true
+  if (String(new Date(a).getTime()) === String(new Date(`${b}`).getTime())) return true
   return false;
 }
 
 export function lte(a, b) {
-  // TODO: ここを実装しなさい
+
+  // 文字列同士の比較
+  if(typeof a == 'string' && typeof b === 'string') {
+    return a <= b;
+  }
+
+  // 数値の比較
+  if (Number(a) <= Number(b)) return true;
+
+  // Dateの比較
+  if (new Date(`${a}`).getTime() < new Date(`${b}`).getTime()) return true;
   return false;
+
+  // objectと配列の比較できてない
 }

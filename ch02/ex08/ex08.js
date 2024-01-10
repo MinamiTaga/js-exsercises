d76a258d5a2a2923de0d6e9ee8d991c9d9827aab
minamitaga <minami.taga@jp.ricoh.com>
2024-01-10 17:13:58 +0900

    fix ch02/08

--- ch02/ex08/ex08.js
+++ ch02/ex08/ex08.js
@@ -1,19 +1,31 @@
+// import { promises as fs } from 'fs';
+
 process.stdin.setEncoding('utf-8');
 let codes = [];
 
 // 入力文字列を１行ずつ配列に入れる
-  for await (const chunk of process.stdin) {
-    codes = chunk.split('\n') 
+for await (const chunk of process.stdin) {
+  codes = chunk.split('\n')
+}
+
+console.log(codes.length)
+
+// 全ての行の末尾に;をつける(空行は除く)
+for (let i = 0; i < codes.length; i++) {
+  let withSemicolons = true;
+
+  // ;がいらない条件
+  if (codes[i].slice(-1) === '(' || codes[i].slice(-1) === '{' || codes[i].slice(-1) === ';' || codes[i] === '') {
+    // if (codes[i].endwith('(') || codes.endwith('{' || codes[i] === '')) {
+    withSemicolons = false;
   }
 
-  console.log(codes.length)
-
-  // 全ての行の末尾に;をつける(空行は除く)
-  for (let i = 0; i<codes.length; i++) {
-  console.log(codes[i])
-    if(codes[i] !== '') {
-      codes[i] = `${codes[i]};`
-    }
+  // セミコロンをつける
+  if (withSemicolons) {
+    codes[i] = `${codes[i]};`
   }
+}
 
-  console.log(codes)
+// fs.writeFile('index.js', codes)
+
+console.log(codes)
--- ch02/ex08/index.js
+++ ch02/ex08/index.js
@@ -1,4 +1,25 @@
 console.log('Hello World')
 console.log('Hello World')
 console.log('Hello World')
-console.log('Hello World')
+
+const method = () => {
+  console.log('Hello World')
+}
+
+class Example {
+
+  constructor(value) {
+    this.value = value;
+  }
+
+  valueOf() {
+    return this.value;
+  }
+
+  toString() {
+    return `${this.value}`;
+  }
+}
+
+let obj = new Example(100000);
+console.log(obj+ 25)

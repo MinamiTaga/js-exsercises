d3f5fa6e423321dcc9762b812848ad897f04d35e
minamitaga <minami.taga@jp.ricoh.com>
2024-07-18 10:43:12 +0900

    add ch14 15

--- ch15.01-03/ex12/idnex.js
+++ ch15.01-03/ex12/idnex.js
@@ -1,3 +1,7 @@
+
+// Backlogの指定のタスクを開く（URLはダミーに変更済み）
+javascript:(function(){t=prompt("課題番号","");if(t!==""&&t!==null){location.href="https://abcd.backlog.com/view/TASK-"+t;}})()
+
 // https://qiita.com/shotets/items/817e2c525615334a38fb
 
 // 選択した文字列を日本語に翻訳

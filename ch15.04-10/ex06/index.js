70a49966d6437ff8f406fb7a9d320068fd079d3f
minamitaga <minami.taga@jp.ricoh.com>
2024-08-18 15:53:46 +0900

    add ex06

--- ch15.04-10/ex06/index.js
+++ ch15.04-10/ex06/index.js
@@ -20,7 +20,40 @@ class TodoApp extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     this.form = this.shadowRoot.querySelector("#new-todo-form");
+    this.input = this.shadowRoot.querySelector("#new-todo");
+    const list = this.shadowRoot.querySelector("#todo-list");
+
     // TODO: 残りを実装
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (this.input.value.trim() === "") {
+        return;
+      }
+      const todo = this.input.value.trim();
+      this.input.value = ""; // textboxを空に
+
+      const li = document.createElement("li");
+      const div = document.createElement("div");
+      const toggle = document.createElement("input");
+      toggle.type = "checkbox";
+      const label = document.createElement("label");
+      const destroy = document.createElement("button");
+      destroy.textContent = "✖";
+
+      toggle.addEventListener("change", () => {
+        li.classList.toggle("completed", toggle.checked);
+      });
+      label.textContent = todo;
+      destroy.addEventListener("click", () => {
+        li.remove();
+      });
+
+      div.appendChild(toggle);
+      div.appendChild(label);
+      div.appendChild(destroy);
+      li.appendChild(div);
+      list.prepend(li);
+    });
   }
 }
 
